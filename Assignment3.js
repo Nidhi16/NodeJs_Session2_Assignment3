995ef656059ca3189a72535ec9d8db7ec2209fb3
Nidhi16 <nidhi.valuable@gmail.com>
2017-10-21 23:28:04 +0530

    Made html and js file for third assignment. created person object and place properties in html document

---
+++ Assignment3.js
@@ -0,0 +1,15 @@
+// Creating person object
+
+var person = new Object();
+
+// Setting properties to person object
+person.firstName = "John";        // Setting first name property
+person.lastName = "Doe";         // Setting last name property
+person.email = "john@doe.com";   // Setting email property
+person.phone = "9878787878";     // Setting phone property
+
+// Take id of each element and set innertext to it
+document.getElementById("first-name").innerText = person.firstName;
+document.getElementById("last-name").innerText = person.lastName;
+document.getElementById("email").innerText = person.email;
+document.getElementById("phone").innerText = person.phone;
